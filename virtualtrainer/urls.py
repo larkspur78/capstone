93a167c28c7147188b7c4ec3fd88b30374512f51
H Porton <larkspur78@yahoo.com>
2016-06-01 19:31:17 -0700

    establish button and database link

--- virtualtrainer/urls.py
+++ virtualtrainer/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.virtualtrainer_home, name='home'),
-    url(r'^musclegroups/$', views.select_musclegroup, name='musclegroups'),
+    url(r'^musclegroups/$', views.search, name='musclegroups'),
     url(r'^workout/$', views.workout, name='workout'),
 ]
--- virtualtrainer/views.py
+++ virtualtrainer/views.py
@@ -8,19 +8,38 @@ def virtualtrainer_home(request):
     return render(request, 'virtualtrainer/home.html')
 
 
-def select_musclegroup(request):
+def search(request):
+    # print(request.GET)
+    # print(request.GET.get('q'))
+    muscle = request.POST.getlist('muscle')
+    # print(muscle)
+    context = {
+        # filters out any posts that can contain the query that we've searched
+        # 'musclegroups': 'muscles'
+        'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
+    }
+    template_location = 'virtualtrainer/musclegroups.html'
+    return render(request, template_location, context)
 
+
+# def select_musclegroup(request):
+#
+#     if request.method == "GET":
+#         muscle = request.GET.getlist('muscle')
+#         # form = SelectMusclesForm(request.POST, instance=muscle)
+#         # if muscle.is_valid():
+#         #     muscle = form.save(commit=False)
+#         #     muscle.save()
+#         return redirect('workout',)
+#     else:
+#     #     muscle = get_object_or_404(MuscleGroup, )
+#         return render(request, 'virtualtrainer/musclegroups.html')
+#
+def workout(request):
     if request.method == "POST":
         muscle = request.POST.getlist('muscle')
-        # form = SelectMusclesForm(request.POST, instance=muscle)
-        # if muscle.is_valid():
-        #     muscle = form.save(commit=False)
-        #     muscle.save()
-        return redirect('workout',)
-    else:
-    #     muscle = get_object_or_404(MuscleGroup, )
-        return render(request, 'virtualtrainer/musclegroups.html')
-
-def workout(request):
-    workout = ['a', 'b']
-    return render(request, 'virtualtrainer/workout.html', {'workout': workout})
+        context = {
+            'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
+        }
+        print(muscle)
+        return render(request, 'virtualtrainer/workout.html', {'workout': Exercise.objects.filter(musclegroup__name__in=muscle)})
