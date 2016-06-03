19980a92c90618747b5e1951414af8d41835acaf
H Porton <larkspur78@yahoo.com>
2016-06-02 21:17:48 -0700

    embed video links

--- virtualtrainer/views.py
+++ virtualtrainer/views.py
@@ -9,17 +9,25 @@ def virtualtrainer_home(request):
 
 
 def search(request):
-    # print(request.GET)
-    # print(request.GET.get('q'))
-    muscle = request.POST.getlist('muscle')
-    # print(muscle)
-    context = {
-        # filters out any posts that can contain the query that we've searched
-        # 'musclegroups': 'muscles'
-        'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
-    }
+    # muscle = request.POST.getlist('muscle')
+    # # print(muscle)
+    # context = {
+    #     # filters out any posts that can contain the query that we've searched
+    #     # 'musclegroups': 'muscles'
+    #     'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
+    # }
     template_location = 'virtualtrainer/musclegroups.html'
-    return render(request, template_location, context)
+    return render(request, template_location,)
+
+
+def workout(request):
+    if request.method == "POST":
+        muscle = request.POST.getlist('muscle')
+        context = {
+            'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
+        }
+        return render(request, 'virtualtrainer/workout.html', context)
+        # {'workout': Exercise.objects.filter(musclegroup__name__in=muscle)})
 
 
 # def select_musclegroup(request):
@@ -35,11 +43,3 @@ def search(request):
 #     #     muscle = get_object_or_404(MuscleGroup, )
 #         return render(request, 'virtualtrainer/musclegroups.html')
 #
-def workout(request):
-    if request.method == "POST":
-        muscle = request.POST.getlist('muscle')
-        context = {
-            'exercises': Exercise.objects.filter(musclegroup__name__in=muscle)
-        }
-        print(muscle)
-        return render(request, 'virtualtrainer/workout.html', {'workout': Exercise.objects.filter(musclegroup__name__in=muscle)})
